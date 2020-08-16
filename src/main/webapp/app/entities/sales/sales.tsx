8ad0a3881dfb371129a2c8fdd4ac7772d6b3060d
Lucas <gomez-lucas@live.com.ar>
2020-08-16 02:03:31 -0300

    sales component with table modified

--- src/main/webapp/app/entities/sales/sales.tsx
+++ src/main/webapp/app/entities/sales/sales.tsx
@@ -1,100 +1,173 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link, RouteComponentProps } from 'react-router-dom';
-import { Button, Col, Row, Table } from 'reactstrap';
 import { Translate, ICrudGetAllAction, TextFormat } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { IRootState } from 'app/shared/reducers';
 import { getEntities } from './sales.reducer';
 import { ISales } from 'app/shared/model/sales.model';
 import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
+import { makeStyles } from '@material-ui/core/styles'
+import Button2 from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import AddIcon from '@material-ui/icons/Add';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import green from '@material-ui/core/colors/green';
+import 'fontsource-roboto';
+
+
 
 export interface ISalesProps extends StateProps, DispatchProps, RouteComponentProps<{ url: string }> {}
 
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 800,
+  },
+  paperStyle: {
+    backgroundColor: "#EBEBEB",
+    padding: "2rem 2rem"
+
+  },
+  headerStyle: {
+    marginBottom: "1rem",
+  },
+  buttonStyle: {
+    backgroundColor: "#004f87",
+    '&:hover': {
+      background: "#5E99C5",
+     },
+  },
+  TableCellStyle: {
+    backgroundColor: "#2a6a9a",
+    color: "#FFFFFF",
+  },
+  RowCellStyle: {
+    backgroundColor: "#FFFFFF",
+    '&:hover': {
+      background: "#5e98c57c",
+    },
+  },
+  EditButton: {
+    color: "green",
+  },
+
+  LinkStyle:{
+    textDecoration: "none",
+  }
+})
+
 export const Sales = (props: ISalesProps) => {
+  const classes = useStyles()
+
+
   useEffect(() => {
     props.getEntities();
   }, []);
 
   const { salesList, match, loading } = props;
   return (
-    <div>
-      <h2 id="sales-heading">
-        <Translate contentKey="testApp.sales.home.title">Sales</Translate>
-        <Link to={`${match.url}/new`} className="btn btn-primary float-right jh-create-entity" id="jh-create-entity">
-          <FontAwesomeIcon icon="plus" />
-          &nbsp;
-          <Translate contentKey="testApp.sales.home.createLabel">Create new Sales</Translate>
-        </Link>
-      </h2>
-      <div className="table-responsive">
+    <div className={classes.paperStyle}>
+
+      <Grid container
+          justify="space-between"
+          alignItems="center"
+          className={classes.headerStyle}
+        >
+          <Grid item>
+            <Typography variant="h4" >
+              Sales
+            </Typography>
+          </Grid>
+          <Grid item>
+          <Link to={`${match.url}/new`} style={{ textDecoration: 'none' }}  >
+            <Button2
+              variant="contained"
+              color="primary"
+              className={classes.buttonStyle}
+              startIcon={<AddIcon />}
+            >
+              Agregar Nueva Venta
+            </Button2>
+            </Link>
+          </Grid>
+        </Grid>
+
+
         {salesList && salesList.length > 0 ? (
-          <Table responsive>
-            <thead>
-              <tr>
-                <th>
-                  <Translate contentKey="global.field.id">ID</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="testApp.sales.description">Description</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="testApp.sales.state">State</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="testApp.sales.date">Date</Translate>
-                </th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>
+          <Paper >
+           <Grid container>
+          <TableContainer component={Paper} >
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell className={classes.TableCellStyle}> <Typography >
+                    ID
+                  </Typography> </TableCell>
+                  <TableCell className={classes.TableCellStyle} align="left"><Typography  >
+                    DESCRIPTION
+                  </Typography> </TableCell>
+                  <TableCell className={classes.TableCellStyle} align="left"><Typography  >
+                    STATE
+                  </Typography> </TableCell>
+                  <TableCell className={classes.TableCellStyle} align="left"><Typography >
+                    DATE
+                  </Typography></TableCell>
+                  <TableCell className={classes.TableCellStyle} align="left"></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+
               {salesList.map((sales, i) => (
-                <tr key={`entity-${i}`}>
-                  <td>
-                    <Button tag={Link} to={`${match.url}/${sales.id}`} color="link" size="sm">
+                  <TableRow key={`entity-${i}`}  className={classes.RowCellStyle} >
+                    <TableCell>
+                      <Link style={{ textDecoration: 'none' }} to={`${match.url}/${sales.id}`}><Typography >
                       {sales.id}
-                    </Button>
-                  </td>
-                  <td>{sales.description}</td>
-                  <td>
-                    <Translate contentKey={`testApp.State.${sales.state}`} />
-                  </td>
-                  <td>{sales.date ? <TextFormat type="date" value={sales.date} format={APP_LOCAL_DATE_FORMAT} /> : null}</td>
-                  <td className="text-right">
-                    <div className="btn-group flex-btn-group-container">
-                      <Button tag={Link} to={`${match.url}/${sales.id}`} color="info" size="sm">
-                        <FontAwesomeIcon icon="eye" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.view">View</Translate>
-                        </span>
-                      </Button>
-                      <Button tag={Link} to={`${match.url}/${sales.id}/edit`} color="primary" size="sm">
-                        <FontAwesomeIcon icon="pencil-alt" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.edit">Edit</Translate>
-                        </span>
-                      </Button>
-                      <Button tag={Link} to={`${match.url}/${sales.id}/delete`} color="danger" size="sm">
-                        <FontAwesomeIcon icon="trash" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.delete">Delete</Translate>
-                        </span>
-                      </Button>
-                    </div>
-                  </td>
-                </tr>
+                      </Typography></Link>
+                    </TableCell>
+                    <TableCell align="left" >{sales.description}</TableCell>
+                    <TableCell align="left" >{sales.state}</TableCell>
+                    <TableCell align="left" > {sales.date ? <TextFormat type="date" value={sales.date} format={APP_LOCAL_DATE_FORMAT} /> : null}</TableCell>
+                    <TableCell align="right" >
+                      <Link to={`${match.url}/${sales.id}`}><IconButton color="primary" aria-label="ver la venta">
+                      <VisibilityIcon />
+                      </IconButton>
+                      </Link>
+
+                      <Link to={`${match.url}/${sales.id}/edit`}><IconButton className={classes.EditButton} aria-label="Editar la venta">
+                      <EditIcon />
+                      </IconButton>
+                      </Link>
+
+                      <Link to={`${match.url}/${sales.id}/delete`} >
+                        <IconButton color="secondary" aria-label="Eliminar  la venta">
+                      <DeleteOutlineIcon />
+                      </IconButton>
+                      </Link>
+                  </TableCell>
+                  </TableRow>
               ))}
-            </tbody>
-          </Table>
-        ) : (
-          !loading && (
-            <div className="alert alert-warning">
-              <Translate contentKey="testApp.sales.home.notFound">No Sales found</Translate>
-            </div>
-          )
-        )}
-      </div>
+                </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        </Paper>
+      )  : (
+                  <div className="alert alert-warning">
+            <Translate contentKey="testApp.sales.home.notFound">No Sales found</Translate>
+          </div>
+      )}
     </div>
   );
 };
